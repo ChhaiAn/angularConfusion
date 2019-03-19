753ddcd564db0bce974a86ea8190ec448d6259e7
Chhai An <chhaian@Chhais-MacBook-Pro.local>
2019-03-19 09:14:27 -0700

    Data Binding

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,10 +1,14 @@
-export interface Dish {
+import { Comment } from './comment'
+
+export class Dish {
   id: string;
   name: string;
   image: string;
   category: string;
   featured: boolean;
+  comments: Comment[];
   label: string;
   price: string;
   description: string;
 }
+
